79caa2ea521f92fd326d6fda6104f60b898006e7
tiktokyi <tiktokyi@gmail.com>
2023-11-08 23:17:32 +0900

    Feat : 퀴즈 맞추면 다음 문제 이동 구현

--- js/buildQuiz.js
+++ js/buildQuiz.js
@@ -1,32 +1,42 @@
 // 퀴즈 정보를 가져와서 퀴즈를 생성하는 함수
-function createQuiz() {
+
+function createQuiz(questionNumber) {
   // 퀴즈를 생성할 요소를 선택합니다.
   var quizElement = document.querySelector(".quiz");
+  console.log(quizInfo[questionNumber][0].correct);
+  console.log(questionNumber);
 
   var quiz_txt = "";
   quiz_txt += '<div class="quizQuestion" >';
-  quiz_txt += "<h2>" + quizInfo[1][0].question + "</h2>";
+  quiz_txt += "<h2>" + quizInfo[questionNumber][0].question + "</h2>";
   quiz_txt += "</div>";
   quiz_txt += '<div class="quizChoice">';
   quiz_txt += '<div class="choice">';
-  quiz_txt += '<img src="./image/playBtn.png">';
-  quiz_txt += "<p>" + quizInfo[1][1].select + "</p>";
+  quiz_txt += '<img src="./image/playBtn.png"  onclick="checkAnswer(1)">';
+  quiz_txt += "<p>" + quizInfo[questionNumber][1].select + "</p>";
   quiz_txt += "</div>";
   quiz_txt += '<div class="choice">';
-  quiz_txt += '<img src="./image/playBtn.png">';
-  quiz_txt += "<p>" + quizInfo[1][2].select + "</p>";
+  quiz_txt += '<img src="./image/playBtn.png"  onclick="checkAnswer(2)">';
+  quiz_txt += "<p>" + quizInfo[questionNumber][2].select + "</p>";
   quiz_txt += "</div>";
   quiz_txt += '<div class="choice">';
-  quiz_txt += '<img src="./image/playBtn.png">';
-  quiz_txt += "<p>" + quizInfo[1][3].select + "<br/></p>";
+  quiz_txt += '<img src="./image/playBtn.png" onclick="checkAnswer(3)">';
+  quiz_txt += "<p>" + quizInfo[questionNumber][3].select + "</p>";
   quiz_txt += "</div>";
   quiz_txt += '<div class="choice">';
-  quiz_txt += '<img src="./image/playBtn.png">';
-  quiz_txt += "<p>" + quizInfo[1][4].select + "<br/></p>";
+  quiz_txt += '<img src="./image/playBtn.png" onclick="checkAnswer(4)">';
+  quiz_txt += "<p>" + quizInfo[questionNumber][4].select + "</p>";
   quiz_txt += "</div>";
   quiz_txt += "</div>";
   document.getElementById("quizWrap").innerHTML = quiz_txt;
 }
 
-// // createQuiz 함수를 호출하여 퀴즈를 생성합니다.
-// createQuiz();
+function checkAnswer(userAnswer) {
+  if (Number(quizInfo[questionNumber][0].correct) === Number(userAnswer)) {
+    console.log("선택지 : " + userAnswer);
+    console.log("퀴즈 정답 : " + quizInfo[questionNumber][0].correct);
+    console.log("정답");
+    questionNumber++;
+    createQuiz(questionNumber);
+  }
+}
