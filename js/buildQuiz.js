d8248f7057e8b4cbe0f4f72d18761ec799269c45
tiktokyi <tiktokyi@gmail.com>
2023-11-09 09:20:12 +0900

    Feat : 퀴즈를 맞추지 못할 경우 풀이와 함께 다음 문제로 이동 버튼 생성

--- js/buildQuiz.js
+++ js/buildQuiz.js
@@ -42,5 +42,36 @@ function checkAnswer(userAnswer) {
     } else {
       createQuiz(questionNumber);
     }
+  } else {
+    solutionQUiz();
   }
+}
+
+function solutionQUiz() {
+  var solution_txt = "";
+  solution_txt += '<div">111</div>';
+
+  var quizWrapElement = document.getElementById("quizWrap");
+  var solutionElement = document.getElementById("quizSolution");
+  quizWrapElement.appendChild(solutionElement); // quizWrap 요소에 solution 요소를 추가
+
+
+  document.getElementById("quizSolution").innerHTML = solution_txt;
+
+  // 다음 퀴즈로 넘어가는 버튼 생성
+  var nextButton = document.createElement("button");
+  nextButton.innerText = "다음 퀴즈로 이동";
+  nextButton.onclick = function () {
+    questionNumber++;
+    if (questionNumber > 3) {
+      window.location.href = "09.html";
+    } else {
+      createQuiz(questionNumber);
+    }
+  };
+
+  // 버튼을 표시할 위치 선택
+  var buttonContainer = document.getElementById("quizWrap");
+  // 버튼을 위치에 추가
+  buttonContainer.appendChild(nextButton);
 }
