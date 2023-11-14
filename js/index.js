6e1b67c73cafe45e8932e0b2e257c72e2bb5b2f4
tiktokyi <tiktokyi@gmail.com>
2023-11-14 11:17:30 +0900

    Fix : 부자연스러운 Index애니메이션 수정

--- js/index.js
+++ js/index.js
@@ -1,10 +1,18 @@
+var indexToggle = "close";
+
 $(document).ready(function () {
     $("#videoIndex").click(function () {
-        var x = $("#index-popup");
-        if (x.hasClass("show")) {
-            x.removeClass("show");
+        if (indexToggle == "close") {
+            indexToggle = "open";
+            $("#index-popup").show();
+            var _temp = $("#index-popup").width();
+            $("#index-popup").stop().animate({ "margin-left": _temp + "px" }, 300);
         } else {
-            x.addClass("show");
+            indexToggle = "close";
+            $("#index-popup").stop().animate({ "margin-left": "0px" }, 300, function () {
+                $("#index-popup").hide();
+            })
+
         }
     });
 });
@@ -14,3 +22,21 @@ $(document).ready(function () {
         $('#lectureWrap').append('<div class="movePage lectureTitle" onclick="location.href=\'' + val.location + '\'">' + val.title + '</div>');
     });
 });
+
+// function index_view() {
+//     if (indexToggle == "close") {
+//         indexToggle = "open";
+//         $("#indexUI").show();
+//         var _temp = $("#index-wrap").width();
+//         $("#index-wrap").stop().animate({ "margin-left": _temp + "px" }, 300);
+//         // $("#index-open-icon").css({"background-position-x":"-"+$("#index-open-icon").width()+"px"});
+//         $("#index-open-txt").html("INDEX").css({ "color": "#FDA947" });
+//     } else {
+//         indexToggle = "close";
+//         $("#index-wrap").stop().animate({ "margin-left": "0px" }, 300, function () {
+//             $("#indexUI").hide();
+//         });
+//         // $("#index-open-icon").css({"background-position-x":"0px"});
+//         $("#index-open-txt").html("INDEX").css({ "color": "#fff" });
+//     }
+// }
