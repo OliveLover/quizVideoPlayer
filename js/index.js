1ef2a8af24aa312e26cb63dcd1faa30d9035cff5
tiktokyi <tiktokyi@gmail.com>
2023-11-16 09:27:14 +0900

    Style : 첫 페이지와 마지막 페이지 버튼 비활성화, 추천영상을 위한 페이지 추가

--- js/index.js
+++ js/index.js
@@ -22,21 +22,3 @@ $(document).ready(function () {
         $('#lectureWrap').append('<div class="movePage lectureTitle" onclick="location.href=\'' + val.location + '\'">' + val.title + '</div>');
     });
 });
-
-// function index_view() {
-//     if (indexToggle == "close") {
-//         indexToggle = "open";
-//         $("#indexUI").show();
-//         var _temp = $("#index-wrap").width();
-//         $("#index-wrap").stop().animate({ "margin-left": _temp + "px" }, 300);
-//         // $("#index-open-icon").css({"background-position-x":"-"+$("#index-open-icon").width()+"px"});
-//         $("#index-open-txt").html("INDEX").css({ "color": "#FDA947" });
-//     } else {
-//         indexToggle = "close";
-//         $("#index-wrap").stop().animate({ "margin-left": "0px" }, 300, function () {
-//             $("#indexUI").hide();
-//         });
-//         // $("#index-open-icon").css({"background-position-x":"0px"});
-//         $("#index-open-txt").html("INDEX").css({ "color": "#fff" });
-//     }
-// }
