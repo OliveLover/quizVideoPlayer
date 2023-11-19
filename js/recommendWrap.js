11a8296f2007baa661da5db8545ab83f1b99165a
tiktokyi <tiktokyi@gmail.com>
2023-11-19 12:47:26 +0900

    Style : 추천강의 영상 페이지 대략적 배치

--- js/recommendWrap.js
+++ js/recommendWrap.js
@@ -8,22 +8,23 @@ function createRecommend() {
     recommend_txt += '봐주셔서 감사합니다.';
     recommend_txt += '</div >';
     recommend_txt += '<div class="recommendLinkWrap">';
-    recommend_txt += '<div class="recommendLink">';
+    recommend_txt += '<div class="recommendPreview">';
     recommend_txt += '<div class="recommendImg">';
     recommend_txt += '<img src="./image/recommend_01.png" style="background-position:">'
     recommend_txt += '</div >';
     recommend_txt += '</div >';
-    recommend_txt += '<div class="recommendLink">';
+    recommend_txt += '<div class="recommendPreview">';
     recommend_txt += '<div class="recommendImg">';
     recommend_txt += '<img src="./image/recommend_01.png" style="background-position:">'
     recommend_txt += '</div >';
     recommend_txt += '</div >';
-    recommend_txt += '<div class="recommendLink">';
+    recommend_txt += '<div class="recommendPreview">';
     recommend_txt += '<div class="recommendImg">';
     recommend_txt += '<img src="./image/recommend_01.png" style="background-position:">'
     recommend_txt += '</div >';
     recommend_txt += '</div > ';
     recommend_txt += '</div >';
+    recommend_txt += '<div class="recommendLink"><a href="https://www.eniedu.co.kr/course" style="text-decoration:none; font-size:30px;">추천영상 링크 바로가기</a></div>';
 
     document.getElementById("recommendWrap").innerHTML = recommend_txt;
 }
