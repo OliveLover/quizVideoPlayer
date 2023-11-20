93d3d20087f9e7ffcfa0188c600c4553071aee58
tiktokyi <tiktokyi@gmail.com>
2023-11-20 18:02:47 +0900

    Style : 추천강의 영상 이미지 추가, 버튼 디자인 수정

--- js/recommendWrap.js
+++ js/recommendWrap.js
@@ -4,9 +4,6 @@
 
 function createRecommend() {
     var recommend_txt = "";
-    recommend_txt += '<div class="recommendTitle">';
-    recommend_txt += '봐주셔서 감사합니다.';
-    recommend_txt += '</div >';
     recommend_txt += '<div class="recommendLinkWrap">';
     recommend_txt += '<div class="recommendPreview">';
     recommend_txt += '<div class="recommendImg">';
@@ -15,12 +12,12 @@ function createRecommend() {
     recommend_txt += '</div >';
     recommend_txt += '<div class="recommendPreview">';
     recommend_txt += '<div class="recommendImg">';
-    recommend_txt += '<img src="./image/recommend_01.png" style="background-position:">'
+    recommend_txt += '<img src="./image/recommend_02.png" style="background-position:">'
     recommend_txt += '</div >';
     recommend_txt += '</div >';
     recommend_txt += '<div class="recommendPreview">';
     recommend_txt += '<div class="recommendImg">';
-    recommend_txt += '<img src="./image/recommend_01.png" style="background-position:">'
+    recommend_txt += '<img src="./image/recommend_03.png" style="background-position:">'
     recommend_txt += '</div >';
     recommend_txt += '</div > ';
     recommend_txt += '</div >';
