bfae014a965c6cb85f39efd5327da29a8641bbe3
tiktokyi <tiktokyi@gmail.com>
2023-11-28 16:10:43 +0900

    Mod : 아이콘 클릭시 화면이동없이 바로 다운로드 되도록 변경

--- js/buildPQuiz.js
+++ js/buildPQuiz.js
@@ -62,7 +62,7 @@ function createPQuiz() {
   /************ 결과별 학습자료 다운로드 ************/
   pQuiz_txt += "<div id='learning-materials'>";
   pQuiz_txt += "<div class='pointWrap'>";
-  pQuiz_txt += "<a href='#' id='downloadLink' download target='_blank'>학습자료 다운로드</a>";
+  pQuiz_txt += "<a href='#' id='downloadLink' download>학습자료 다운로드</a>";
   pQuiz_txt += "<div style='width:140px; height:1px; border-bottom:2px solid;margin-top:4px;'></div>";
   pQuiz_txt += "</div>";
   pQuiz_txt += "<div class='pointImgWrap'>";
